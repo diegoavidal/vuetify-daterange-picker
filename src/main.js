47f47d79cac92de6de72b1d2231774ec5003fb59
Chris Zimmerman <czimmerman@kbsi-cs.com>
2019-08-08 08:48:22 -0500

    fixed for vuetify 2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
-import './plugins/vuetify';
+import vuetify from './plugins/vuetify';
 import App from './App.vue';
 Vue.config.productionTip = false;
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
